cc03e0468033705ac55d40b7a44f2bf9d26b4e73
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2022-04-03 21:21:10 +0100

    added start with react and wasm setup

---
+++ wasm_react/README.md
@@ -0,0 +1,21 @@
+# {{package_name}}
+
+
+## Initial Package Skeleton Setup
+
+Install required extension for cargo if needed:
+
+```sh
+cargo install wasm-bindgen-cli
+```
+and required target if not present:
+
+```sh
+make add__wasm
+```
+
+to start server and install everything execute:
+
+```sh
+make start
+```
--- wasm_react/src/index.jsx
+++ wasm_react/src/index.jsx
@@ -1,4 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(<h1> Hello, World!</h1>, document.getElementById("root"));
+
+const wasm = import("../build/{{crate_name}}");
+
+wasm.then((m) => {
+  const App = () => {
+    const [name, setName] = useState("");
+    const handleChange = (e) => {
+      setName(e.target.value);
+    };
+    const handleClick = () => {
+      m.welcome(name);
+    };
+
+    return (
+      <>
+        <div>
+          <h1>Hi there</h1>
+          <button onClick={m.big_computation}>Run Computation</button>
+        </div>
+        <div>
+          <input type="text" onChange={handleChange} />
+          <button onClick={handleClick}>Say hello!</button>
+        </div>
+      </>
+    );
+  };
+
+  // This is necessary to drive the wasm promise to completion
+  // as otherwise it wont be called
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
